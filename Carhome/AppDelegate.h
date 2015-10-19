0b88502446b294f5e6d9afb8ce0cc6480c2c53e0
wz <327539627@qq.com>
2015-10-19 21:02:13 +0800

    创建工程

---
+++ Carhome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Carhome
+//
+//  Created by wz on 15-10-19.
+//  Copyright (c) 2015年 wz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Carhome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Carhome
+//
+//  Created by wz on 15-10-19.
+//  Copyright (c) 2015年 wz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
